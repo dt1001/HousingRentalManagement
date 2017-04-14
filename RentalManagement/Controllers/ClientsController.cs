e4223336ff8b554894b1ddaf1928be57a6fad090
jakejkim <jakejkim11@gmail.com>
2017-04-13 18:07:55 -0700

    Fixed client edit function

--- RentalManagement/Controllers/ClientsController.cs
+++ RentalManagement/Controllers/ClientsController.cs
@@ -17,16 +17,21 @@ namespace RentalManagement.Controllers {
         [Authorize(Roles = "Employee,Supervisor,Client")]
         public ActionResult Index() {
             List<AssetClientViewModel> viewModel = new List<AssetClientViewModel>();
-            var clients = (from o in db.OccupancyRecords 
+            var clients = (from o in db.OccupancyRecords
                            join c in db.Clients on o.ClientId equals c.Id
                            join a in db.Assets on o.AssetId equals a.Id
                            select new {
                                ClientId = c.Id,
+                               AssetId = a.Id,
                                Name = c.Name,
                                AssetType = a.Type,
-                               AssetAddress = a.Address }).Distinct().ToList();
+                               AssetAddress = a.Address,
+                               StartDate = o.StartDate
+                           }).ToList();
 
-            foreach (var client in clients) {
+            var temps = clients.GroupBy(x => x.ClientId).Select(g => g.LastOrDefault()).ToList();
+
+            foreach (var client in temps) {
                 viewModel.Add(new AssetClientViewModel() {
                     ClientId = client.ClientId,
                     ClientName = client.Name,
@@ -39,12 +44,27 @@ namespace RentalManagement.Controllers {
 
         // GET: Clients/Details/5
         public ActionResult Details(int id) {
-            var clients = db.Clients.Include(c => c.OccupancyRecords).Include(c => c.HomeAddress).Include(c => c.WorkAddress).SingleOrDefault(c => c.Id == id);
-            if (clients == null) {
-                return HttpNotFound();
-            }
+            //List<AssetClientViewModel> viewModel = new List<AssetClientViewModel>();
+            var clients = (from o in db.OccupancyRecords
+                           join c in db.Clients on o.ClientId equals c.Id
+                           join a in db.Assets on o.AssetId equals a.Id
+                           select new {
+                               ClientId = id,
+                               Name = c.Name,
+                               AssetType = a.Type,
+                               ClientHomeAddress = c.HomeAddress,
+                               ClientWorkAddress = c.WorkAddress
+                           }).Distinct().FirstOrDefault(c => c.ClientId == id);
 
-            return View(clients);
+            var viewModel = new AssetClientViewModel {
+                ClientId = clients.ClientId,
+                ClientName = clients.Name,
+                AssetType = clients.AssetType,
+                ClientHomeAddress = clients.ClientHomeAddress,
+                ClientWorkAddress = clients.ClientWorkAddress
+            };
+
+            return View(viewModel);
         }
 
         // GET: Clients/Create
